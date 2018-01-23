4679cfdc4d3a4a75316405d186c32703c24460df
Zukka <info@zukkadev.net>
2018-01-23 12:04:27 +0100

    Add a Dollar class for fix the test (Red -> Green)

---
+++ Multi Currency/Dollar.swift
@@ -0,0 +1,24 @@
+//
+//  Dollar.swift
+//  Multi Currency
+//
+//  Created by Alessandro Bellotti on 23/01/18.
+//  Copyright © 2018 Alessandro Bellotti. All rights reserved.
+//
+
+import Foundation
+
+class Dollar {
+    
+    var amount: Int
+    
+    init(amount: Int) {
+        self.amount = amount
+    }
+    
+    func times(multipler: Int) {
+        self.amount = self.amount * multipler
+    }
+    
+    
+}
